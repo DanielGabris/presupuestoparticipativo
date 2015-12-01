ae0f132dbd8fa420b15636e006992ee0946a94ca
Miguel Prados <miguelpradosrodriguez@gmail.com>
2015-12-01 13:19:32 +0100

    Update jquery.validationEngine-es.js

--- app/assets/javascripts/individual/jquery.validationEngine-es.js
+++ app/assets/javascripts/individual/jquery.validationEngine-es.js
@@ -6,125 +6,125 @@
             $.validationEngineLanguage.allRules = {
                 "postalCodeCA": {
                     "regex": /^[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] ?[0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]$/i,
-                    "alertText": "* Code postal invalide (ex. : H0H 0H0)"
+                    "alertText": "* Código postal erroneo"
                 },
                 "required": {
                     "regex": "none",
-                    "alertText": "* Ce champ est requis",
-                    "alertTextCheckboxMultiple": "* Choisir une option",
-                    "alertTextCheckboxe": "* Cette option est requise"
+                    "alertText": "* Se requiere este campo",
+                    "alertTextCheckboxMultiple": "* Elija una opción",
+                    "alertTextCheckboxe": "* Se requiere esta opción"
                 },
                 "requiredInFunction": {
                     "func": function(field, rules, i, options){
                         return (field.val() == "test") ? true : false;
                     },
-                    "alertText": "* Field must equal test"
+                    "alertText": "* El campo debe igualar el test"
                 },
                 "minSize": {
                     "regex": "none",
-                    "alertText": "* Minimum ",
-                    "alertText2": " caractères requis"
+                    "alertText": "* Mínimo ",
+                    "alertText2": " caracteres requeridos"
                 },
                 "groupRequired": {
                     "regex": "none",
-                    "alertText": "* Vous devez remplir un des champs suivant"
+                    "alertText": "* Debe rellenar uno de los campos siguientes"
                 },
                 "maxSize": {
                     "regex": "none",
-                    "alertText": "* Maximum ",
-                    "alertText2": " caractères requis"
+                    "alertText": "* Máximo ",
+                    "alertText2": " caracteres requeridos"
                 },
                 "min": {
                     "regex": "none",
-                    "alertText": "* Valeur minimum requise "
+                    "alertText": "* Valor máximo requerido "
                 },
                 "max": {
                     "regex": "none",
-                    "alertText": "* Valeur maximum requise "
+                    "alertText": "* Valor mínimo requerido "
                 },
                 "past": {
                     "regex": "none",
-                    "alertText": "* Date antérieure au "
+                    "alertText": "* Fecha anterior a "
                 },
                 "future": {
                     "regex": "none",
-                    "alertText": "* Date postérieure au "
+                    "alertText": "* Fecha posterior a "
                 },
                 "maxCheckbox": {
                     "regex": "none",
-                    "alertText": "* Nombre max de choix excédé"
+                    "alertText": "* Número máximo de opciones excedidas"
                 },
                 "minCheckbox": {
                     "regex": "none",
-                    "alertText": "* Veuillez choisir ",
-                    "alertText2": " options"
+                    "alertText": "* Por favor, elija ",
+                    "alertText2": " optiones"
                 },
                 "equals": {
                     "regex": "none",
-                    "alertText": "* Votre champ n'est pas identique"
+                    "alertText": "* Su campo no es idéntico"
                 },
                 "creditCard": {
                     "regex": "none",
-                    "alertText": "* Numéro de carte bancaire valide"
+                    "alertText": "* Número de tarjeta de crédito correcto"
                 },
                 "phone": {
                     // credit: jquery.h5validate.js / orefalo
                     "regex": /^([\+][0-9]{1,3}([ \.\-])?)?([\(][0-9]{1,6}[\)])?([0-9 \.\-]{1,32})(([A-Za-z \:]{1,11})?[0-9]{1,4}?)$/,
-                    "alertText": "* Numéro de téléphone invalide"
+                    "alertText": "* Número de teléfono inválido"
                 },
                 "email": {
                     // Shamelessly lifted from Scott Gonzalez via the Bassistance Validation plugin http://projects.scottsplayground.com/email_address_validation/
                     "regex": /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,
-                    "alertText": "* Adresse email invalide"
+                    "alertText": "* Email incorrecto"
                 },
                 "integer": {
                     "regex": /^[\-\+]?\d+$/,
-                    "alertText": "* Nombre entier invalide"
+                    "alertText": "* Número entero válido"
                 },
                 "number": {
                     // Number, including positive, negative, and floating decimal. credit: orefalo
                     "regex": /^[\-\+]?((([0-9]{1,3})([,][0-9]{3})*)|([0-9]+))?([\.]([0-9]+))?$/,
-                    "alertText": "* Nombre flottant invalide"
+                    "alertText": "* Número decimal válido"
                 },
                 "date": {
                     "regex": /^\d{4}[\/\-](0?[1-9]|1[012])[\/\-](0?[1-9]|[12][0-9]|3[01])$/,
-                    "alertText": "* Date invalide, format YYYY-MM-DD requis"
+                    "alertText": "* Fecha inválida, formato YYYY-MM-DD requerido"
                 },
                 "ipv4": {
                     "regex": /^((([01]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))[.]){3}(([0-1]?[0-9]{1,2})|(2[0-4][0-9])|(25[0-5]))$/,
-                    "alertText": "* Adresse IP invalide"
+                    "alertText": "* Dirección de IP inválida"
                 },
                 "url": {
                     "regex": /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i,
-                    "alertText": "* URL invalide"
+                    "alertText": "* URL inválida"
                 },
                 "onlyNumberSp": {
                     "regex": /^[0-9\ ]+$/,
-                    "alertText": "* Seuls les chiffres sont acceptés"
+                    "alertText": "* Solo se aceptan números"
                 },
                 "onlyLetterSp": {
                     "regex": /^[a-zA-Z\u00C0-\u00D6\u00D9-\u00F6\u00F9-\u00FD\ \']+$/,
-                    "alertText": "* Seules les lettres sont acceptées"
+                    "alertText": "* Solo se aceptan letras"
                 },
                 "onlyLetterNumber": {
                     "regex": /^[0-9a-zA-Z\u00C0-\u00D6\u00D9-\u00F6\u00F9-\u00FD]+$/,
-                    "alertText": "* Aucun caractère spécial n'est accepté"
+                    "alertText": "* No se aceptan caracteres especiales"
                 },
                 // --- CUSTOM RULES -- Those are specific to the demos, they can be removed or changed to your likings
                 "ajaxUserCall": {
                     "url": "ajaxValidateFieldUser",
                     "extraData": "name=eric",
-                    "alertTextLoad": "* Chargement, veuillez attendre",
-                    "alertText": "* Ce nom est déjà pris"
+                    "alertTextLoad": "* Cargando , por favor espere",
+                    "alertText": "* Este nombre ya está en uso"
                 },
                 "ajaxNameCall": {
                     "url": "ajaxValidateFieldName",
-                    "alertText": "* Ce nom est déjà pris",
-                    "alertTextOk": "*Ce nom est disponible",
-                    "alertTextLoad": "* Chargement, veuillez attendre"
+                    "alertText": "* Este nombre ya está en uso",
+                    "alertTextOk": "*Este nombre está disponible",
+                    "alertTextLoad": "* Cargando , por favor espere"
                 },
                 "validate2fields": {
-                    "alertText": "Veuillez taper le mot HELLO"
+                    "alertText": "Por favor escriba la palabra HOLA"
                 }
             };
         }
